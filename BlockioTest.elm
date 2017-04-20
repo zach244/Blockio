f8c074c5e703bf6310f0b4dc31330f496a8cc475
Yetti95 <yetti95@gmail.com>
2017-04-19 22:56:23 -0600

    Blockio Lives. But Goes in reverse
    Jumps down and no horizontal movements

--- BlockioTest.elm
+++ BlockioTest.elm
@@ -9,6 +9,7 @@ import Collision2D
 
 
 
+
 main = program
     { init = (initialModel, Cmd.none),
       view = view,
@@ -22,6 +23,8 @@ type KeyTypes = KeyUp Int | KeyDown Int
 initialModel = {x = 0, vx = 0,
                 y = 0, vy = 0,
                 blockioAcceleration = 0}
+
+--door = (Svg.rect [x "0", y "0", width "25", height "25"] [])
 
 subscriptions model =
   Sub.batch
@@ -39,7 +42,7 @@ update msg model =
     Tick time -> tick model
 
 runSpeed = 4
-gravityS = 0.5
+gravityS = 0.2
 jumpSpeed = 5
 
 key msg model =
@@ -56,9 +59,15 @@ key msg model =
         KeyDown 68 -> run runSpeed
         KeyUp 68 -> stop (>)
 
+        --S key
+        KeyDown 83 -> fastFall 3
+
         _ -> identity
 
 run moveSpeed model =
+  {model | blockioAcceleration = moveSpeed}
+
+fastFall moveSpeed model =
   {model | blockioAcceleration = moveSpeed}
 
 jump model =
@@ -73,6 +82,12 @@ stop condition model =
   else
     model
 
+--Blockio's hit box using collision
+--rectangle takes centerx, centery, width, height
+--centerx = model.x
+--centery = model.y
+--width = width "given in view's svg dimensions"
+--height = height "   "
 
 tick model =
     model
@@ -93,18 +108,27 @@ floor model =
        {model | y = 0, vy = 0}
     else
        model
-
+--kill function should be very similar to floor and added to tick
 view model =
+
+
     svg
       -- List of attributes of SVG node
       [ width "100%", height "100%" -- Scale up to take the full page
       , viewBox "0 0 500 100" -- Numbers used in the drawing are relative to these viewBox dimensions. 0 in the y dimension is the top of the drawing.
       ]
       -- List of children
-      [ circle [ cx (toString (model.x + 16))
-               , cy (toString (75 - model.y))
-               , r "16"] []
+      [ rect [
+          x (toString model.x)
+          , y (toString (model.y + 40))
+          , width "50"
+          , height "50"
+      ] []
       , line [ x1 "0", y1 "95"
              , x2 "500", y2 "95"
              , strokeWidth "8"
-             , stroke "black" ] [] ]
+             , stroke "black" ] []]
+  --      rect [x "0",
+    --          y "0",
+      --        width "25",
+        --      height "100", animateColor (List Svg.Attribute msg) (List Svg.Svg msg)] [] ]
