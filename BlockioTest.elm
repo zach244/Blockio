d1b67db1f6ca9a2faee2a71b422ec9a5747d7303
Yetti95 <yetti95@gmail.com>
2017-04-27 16:36:15 -0600

    gameOver, minor collisions with the first spike

--- BlockioTest.elm
+++ BlockioTest.elm
@@ -88,24 +88,24 @@ key msg model =
         _ -> identity
 
 run moveSpeed model =
-  let modelBlockio = blockio.blockioAcceleration in
-    {model | modelBlockio = moveSpeed}
+  let b = model.blockio in
+    {model | blockio = {b | blockioAcceleration = moveSpeed}}
 
 fastFall moveSpeed model =
-  let modelBlockio = blockio.vy in
-    {model | modelBlockio = (model.blockio.vy - moveSpeed)}
+  let b = model.blockio in
+    {model | blockio = {b | vy = (model.blockio.vy - moveSpeed)}}
 
 jump model =
   if model.blockio.vy == 0 then
-    let modelBlockio = blockio.vy in
-      {model | modelBlockio = jumpSpeed}
+    let b = model.blockio in
+      {model | blockio = {b | vy = jumpSpeed}}
   else
     model
 
 stop condition model =
   if condition model.blockio.blockioAcceleration 0 then
-    let modelBlockio = blockio.blockioAcceleration in
-      {model | modelBlockio = 0}
+    let b = model.blockio in
+      {model | blockio = {b | blockioAcceleration = 0}}
   else
     model
 
@@ -119,36 +119,44 @@ stop condition model =
 tick model =
     model
         -- |>kill
-        |>acceleration
+        |> gameOver
+        |> spikeAirCollision
+        |> spikeGroundCollision
+        |> acceleration
         |> gravity
         |> motion
         |> floor
 -- alienHitBox model =
 
 gravity model =
-  let modelBlockio = blockio.vy in
-  {model | modelBlockio = model.blockio.vy - gravityS}
+  let b = model.blockio in
+  {model | blockio = {b | vy = model.blockio.vy - gravityS}}
 
 motion model =
-  {model | blockio.x = model.blockio.x + model.blockio.vx,
-          blockio.y = model.blockio.y + model.blockio.vy
+  let b = model.blockio in
+    {model | blockio = {b | x = model.blockio.x + model.blockio.vx,
+                            y = model.blockio.y + model.blockio.vy
+                          }
   }
 acceleration model =
-  {model | blockio.vx = model.blockio.blockioAcceleration}
+  let b = model.blockio in
+    {model | blockio = {b | vx = model.blockio.blockioAcceleration}}
 
 floor model =
     if model.blockio.y < 0 then
-       {model | blockio.y = 0, blockio.vy = 0}
+      let b = model.blockio in
+       {model | blockio = {b | y = 0, vy = 0}}
     else
        model
 
 motionX model =
-  let modelBlockio = blockio.x in
-    {model | modelBlockio = model.blockio.x + model.blockio.vx}
+  let b = model.blockio in
+    {model | blockio = {b | x = model.blockio.x + model.blockio.vx}}
 
 motionY model =
-  let modelBlockio = blockio.blockioAcceleration in
-    {model | blockio.x = model.blockio.x + model.blockio.vx
+  let b = model.blockio in
+    {model | blockio = {b | y = model.blockio.y + model.blockio.vy}}
+
 
 -- kill model =
 --   if (axisAlignedBoundingBox r1 r2) then
@@ -156,15 +164,44 @@ motionY model =
 --kill function should be very similar to floor and added to tick
 adjustY model =
   let b = model.blockio in
-  {model | blockio = {b | y = (model.blockio.y - 255)}
+    {model | blockio = {b | y = (model.blockio.y - 255)}}
 
 adjustX model =
-  {model | blockio.x = (model.blockio.x - 445)}
-view model =
+  let b = model.blockio in
+  {model | blockio = { b | x = (model.blockio.x - 445)}}
+
+spikeGroundCollision model =
+  if (model.blockio.x - 445) == 5 && (model.blockio.y - 270) == 10 then
+    let b = model.blockio in
+      {model | blockio = { b | x = 0,
+                              y = 0,
+                              lives = (model.blockio.lives - 1)}}
+  else
+    model
+
+spikeAirCollision model =
+  if (model.blockio.x - 445) == 5 && (model.blockio.y - 255) == 5 then
+    let b = model.blockio in
+      {model | blockio = { b | x = 0,
+                              y = 0,
+                              lives = (model.blockio.lives - 1)}}
+
+  else model
+gameOver model =
+  if (model.blockio.lives) == 0 then
     toHtml(
       collage 1000 500 [(
+        (toForm (centered (fromString ("GameOver"))))
+      )]
+    )
+  else
+    model
+
+view model =
+    toHtml(
+      collage 2000 500 [(
         (moveY (model.blockio.y - 230) (moveX (model.blockio.x - 445) (filled (black ) (rect 25 25))))),
-         (moveY -250 (filled (black ) (rect 1000 20))),
+         (moveY -250 (filled (black ) (rect 2000 20))),
         -- line is created for the floor
          (rotate (degrees 330)(moveY -245 (moveX 0 (filled (black ) (ngon 3 15))))),
          (rotate (degrees 330)(moveY -245 (moveX 30 (filled (black ) (ngon 3 15))))),
